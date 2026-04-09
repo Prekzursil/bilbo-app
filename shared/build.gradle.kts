82120f6a366e3a8b4da511f3958ad88a97eff99e
Prekzursil <prekzursil1993@gmail.com>
2026-04-09 07:53:32 +0000

    fix: add XCFramework import, simplify CodeQL to JS/TS only
    - Add missing import for org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
    - Remove Kotlin from CodeQL (requires autobuild which fails without iOS targets on ubuntu)
    - Keep JS/TS CodeQL analysis for Edge Functions security scanning

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
